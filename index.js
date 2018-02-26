741ea07a8255281ca4144876a752d59c9ccb163a
Christian Eichele <christian.eichele@tum.de>
2018-02-26 15:56:27 +0100

    Hello World Skill

---
+++ index.js
@@ -0,0 +1,17 @@
+const Alexa = require('alexa-sdk');
+var Config = require('./config/config');
+
+exports.handler = function (event, context, callback) {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.appId = Config.APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+const handlers = {
+    'HelloWorldIntent': function () {
+        //build response first using responseBuilder and then emit
+        this.response.speak('Hello World!');
+        this.emit(':responseReady');
+    }
+};
